9141b43d308db18a008f8d3e2c4494c19ddbed5b
Thimo Braker <thibmorozier@live.nl>
2018-09-21 14:30:52 +0200

    Add test app

---
+++ examples/ExampleApp/src/middleware.php
@@ -0,0 +1,8 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+declare(strict_types = 1);
+namespace ExampleApp;
+
+use \adrianfalleiro\SlimCLIRunner;
+
+$app->add(SlimCLIRunner::class);
